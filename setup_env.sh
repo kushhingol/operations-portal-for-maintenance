c0e920a47f97428ae23df3feb4b44f1f8f3eeaa0
Kush Hingol <kushhingol.kh@gmail.com>
2022-07-06 19:08:29 +0530

    updayed setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 FILENAME=".env"
-rm $FILENAME
+if test -f "$FILENAME"; then
+   rm $FILENAME
+fi
+
 touch $FILENAME
 echo REACT_APP_API_URL=https://rugged-badlands-76158.herokuapp.com >> $FILENAME
